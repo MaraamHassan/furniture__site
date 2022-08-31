e7cc8555b87f864f1661c105d74e4707cd95237a
MaraamHassan <108126157+MaraamHassan@users.noreply.github.com>
2022-08-31 19:22:36 +0200

    tables and chairs

---
+++ js/exam.js
@@ -0,0 +1,16 @@
+var x = prompt("enter your profession case")
+                                        
+switch(x){
+    case "frontend" :
+    console.log("salary is 6000")
+    break;
+
+    case "backend" :
+    console.log("salary is 7500")
+    break;
+
+    case "full" :
+    console.log("salary is 10000")
+    break;
+
+}
